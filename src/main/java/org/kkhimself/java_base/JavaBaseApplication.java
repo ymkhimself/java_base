e6677ab7f65c3cc8bc29da38f35ee2dbdcea7959
ymk <1766273199@qq.com>
2024-05-14 23:40:09 +0800

    init sprint-boot project

---
+++ src/main/java/org/kkhimself/java_base/JavaBaseApplication.java
@@ -0,0 +1,13 @@
+package org.kkhimself.java_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kkhimself/java_base/JavaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kkhimself.java_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
