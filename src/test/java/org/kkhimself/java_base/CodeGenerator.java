ed5b59efb03da54748d802f816bcd092dba1472b
ymk <1766273199@qq.com>
2024-05-17 01:06:52 +0800

    feat:写好代码生成器

--- src/test/java/org/kkhimself/java_base/CodeGenerator.java
+++ src/test/java/org/kkhimself/java_base/CodeGenerator.java
@@ -1,6 +1,7 @@
 package org.kkhimself.java_base;
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.TemplateType;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.nio.file.Paths;
@@ -10,17 +11,21 @@ public class CodeGenerator {
         FastAutoGenerator.create("jdbc:mysql://localhost:3306/test", "root", "Ymk0910!")
                 .globalConfig(builder -> builder
                         .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java/")
-                        .commentDate("yyyy-MM-dd")
+                        .commentDate("yyyy-MM-dd").disableOpenDir()
                 )
                 .packageConfig(builder -> builder
                         .parent("org.kkhimself.java_base")
                         .entity("model")
                         .mapper("dao")
+                        .service("service")
                 )
                 .strategyConfig(builder -> builder
                         .entityBuilder()
                         .enableLombok()
+                        .serviceBuilder().formatServiceFileName("%sService")
+                        .mapperBuilder().disableMapperXml()
                 )
+                .templateConfig(builder -> builder.disable(TemplateType.XML))
                 .templateEngine(new FreemarkerTemplateEngine())
                 .execute();
     }
